c04b22ea5132fd3d8523f028a555aed9bf638bc7
Jorge Luis <jorgeluis17rh17@gmail.com>
2021-04-20 23:15:24 -0500

    aea5

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM php:7.4-apache
 
 COPY . /var/www/html
 
+RUN docker-php-ext-install mysqli
+
 EXPOSE 80 
--- includes/config/database.php
+++ includes/config/database.php
@@ -2,6 +2,7 @@
 
 function conectarBD() : mysqli {
     $db = mysqli_connect('bienes-raices.c8ayhidelcug.us-east-1.rds.amazonaws.com', 'admin', '12345678','bienes_raices');
+    //$db = mysqli_connect('localhost', 'root', 'root','bienes_raices');
 
     if(!$db){
         echo "Error no se pudo conectar";
