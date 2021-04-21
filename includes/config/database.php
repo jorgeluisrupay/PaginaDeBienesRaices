605e6a99bca5fd79ed3442db7ecdf4bd291a0977
Jorge Luis <jorgeluis17rh17@gmail.com>
2021-04-20 22:59:12 -0500

    aea4

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectarBD() : mysqli {
-    $db = mysqli_connect('localhost', 'root', 'root','bienes_raices');
+    $db = mysqli_connect('bienes-raices.c8ayhidelcug.us-east-1.rds.amazonaws.com', 'admin', '12345678','bienes_raices');
 
     if(!$db){
         echo "Error no se pudo conectar";
